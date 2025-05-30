81bb498fb8bf19cb1a9617b00a60ca56a5d245cd
gankdemon <michael.bchd@gmail.com>
2025-05-30 09:51:33 -0400

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -7,9 +7,12 @@ end
 
 local staffUrl = "https://raw.githubusercontent.com/gankdemon/Northwind/main/StaffTracker.lua"
 local peltUrl  = "https://raw.githubusercontent.com/gankdemon/Northwind/main/PeltTracker.lua"
+local teleporterUrl = "https://raw.githubusercontent.com/gankdemon/Northwind/main/teleporter.lua"
 
 local Staff = fetchModule(staffUrl)
 local Pelt  = fetchModule(peltUrl)
+local Teleporter = fetchModule(teleporterUrl)
 
 Staff.init()
 Pelt.init()
+Teleporter.init()
